034f94b5a3e8584ef22e6286f978733e8797b3db
Aliaksandr Lutsevich <lutzby@gmail.com>
2024-07-09 14:56:57 +0000

    modified:   mean_var_std.py seems to work as intended, but it fails test due to dict to dict math operations. could not figure out is this a test script issue

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,48 @@
 import numpy as np
 
 def calculate(list):
+    # checking for list length
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
+    
+    # list to matrix conversion
+    matrix = np.array(list).reshape(3,3)
 
+    # creating dictionary for results
+    calculations = {
+        'mean': [],
+        'variance': [],
+        'standart deviation': [],
+        'max': [],
+        'min': [],
+        'sum': []
+    }
 
+    # calculations for columns (axis 0)
+    calculations['mean'].append(np.mean(matrix, axis=0).tolist())
+    calculations['variance'].append(np.var(matrix, axis=0).tolist())
+    calculations['standart deviation'].append(np.std(matrix, axis=0).tolist())
+    calculations['max'].append(np.max(matrix, axis=0).tolist())
+    calculations['min'].append(np.min(matrix, axis=0).tolist())
+    calculations['sum'].append(np.sum(matrix, axis=0).tolist())
 
+    # calculations for rows (axis 1)
+    calculations['mean'].append(np.mean(matrix, axis=1).tolist())
+    calculations['variance'].append(np.var(matrix, axis=1).tolist())
+    calculations['standart deviation'].append(np.std(matrix, axis=1).tolist())
+    calculations['max'].append(np.max(matrix, axis=1).tolist())
+    calculations['min'].append(np.min(matrix, axis=1).tolist())
+    calculations['sum'].append(np.sum(matrix, axis=1).tolist())
+
+    # calculations for flattened matrix
+    calculations['mean'].append(np.mean(matrix).tolist())
+    calculations['variance'].append(np.var(matrix).tolist())
+    calculations['standart deviation'].append(np.std(matrix).tolist())
+    calculations['max'].append(np.max(matrix).tolist())
+    calculations['min'].append(np.min(matrix).tolist())
+    calculations['sum'].append(np.sum(matrix).tolist())
 
     return calculations
+
+#result = calculate([0,1,2,3,4,5,6,7,8])
+#print(result)
